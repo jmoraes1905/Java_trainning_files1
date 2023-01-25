41dbfa114635fbeab4321773e07c95614987f804
Joel  Godoy <j.godoy.moraes@gmail.com>
2023-01-25 10:14:15 -0300

    Writing text info over a file

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,28 +1,28 @@
 package Application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String path = "c:\\temp\\in.txt";
-		//Eliminate declaration of file related objects by using block try-with-resources
-		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
-			//Declaration and instantiation of buffered reader and file reader objects done at try block start 
-			String line = br.readLine(); //String receives text file lines until file is over 
-			while(line!=null) {
-				System.out.println(line);
-				line = br.readLine();
+		String[] lines = new String[] {"Good morning","Good afternoon","Good evenning"};
+		String path = "c:\\temp\\out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){ //true parameter adds written info without overwriting previous ones
+			for(String line: lines) {
+				bw.write(line);
+				bw.newLine();
 			}
 		}
 		catch(IOException e) {
-			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
 		}
 		
-		
+	
+	
 	}
 
 }
